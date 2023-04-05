a844f4306018f9e2ecee885c9d73d22593f43e5d
Walderley De Oliveira Santos Neto <woneto@stefanini.com>
2023-04-05 19:48:37 -0300

    alteração na estrutura do projeto

--- testeConversoes/api/api.go
+++ testeConversoes/api/api.go
@@ -1,41 +1,80 @@
-func exchangeHandler(w http.ResponseWriter, r *http.Request) {
+package main
+
+import (
+    "database/sql"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "strconv"
+    "time"
+
+    "github.com/gorilla/mux"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/ericlagergren/decimal"
+)
+
+type Conversion struct {
+    ID              int64           `json:"id"`
+    FromCurrency    string          `json:"fromCurrency"`
+    ToCurrency      string          `json:"toCurrency"`
+    Rate            decimal.Decimal `json:"rate"`
+    Amount          decimal.Decimal `json:"amount"`
+    ConvertedAmount decimal.Decimal `json:"convertedAmount"`
+    CreatedAt       time.Time       `json:"createdAt"`
+}
+
+func main() {
+    // Configurar o roteador da API
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/exchange/{amount}/{from}/{to}/{rate}", handleExchange)
+
+    // Configurar o banco de dados
+    db, err := sql.Open("mysql", "user:password@/database")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Verificar a conexão com o banco de dados
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Iniciar o servidor HTTP
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func handleExchange(w http.ResponseWriter, r *http.Request) {
+    // Obter os parâmetros da URL
     vars := mux.Vars(r)
     amount, _ := strconv.ParseFloat(vars["amount"], 64)
     from := vars["from"]
     to := vars["to"]
-    rate, _ := strconv.ParseFloat(vars["rate"], 64)
+    rate, _ := decimal.NewFromString(vars["rate"])
 
-    var result float64
-
-    switch {
-    case from == "BRL" && to == "USD":
-        result = amount / rate
-    case from == "USD" && to == "BRL":
-        result = amount * rate
-    case from == "BRL" && to == "EUR":
-        result = amount / rate
-    case from == "EUR" && to == "BRL":
-        result = amount * rate
-    case from == "BTC" && to == "USD":
-        result = amount * rate
-    case from == "USD" && to == "BTC":
-        result = amount / rate
-    case from == "BTC" && to == "BRL":
-        result = amount * rate
-    case from == "BRL" && to == "BTC":
-        result = amount / rate
-    default:
-        http.Error(w, "Conversão não suportada", http.StatusBadRequest)
+    // Calcular a conversão de moedas
+    var convertedAmount decimal.Decimal
+    if from == "BRL" && to == "USD" {
+        convertedAmount = decimal.NewFromFloat(amount).Mul(rate)
+    } else if from == "USD" && to == "BRL" {
+        convertedAmount = decimal.NewFromFloat(amount).Div(rate)
+    } else if from == "BRL" && to == "EUR" {
+        convertedAmount = decimal.NewFromFloat(amount).Mul(rate)
+    } else if from == "EUR" && to == "BRL" {
+        convertedAmount = decimal.NewFromFloat(amount).Div(rate)
+    } else if from == "BTC" && to == "USD" {
+        convertedAmount = decimal.NewFromFloat(amount).Mul(rate)
+    } else if from == "BTC" && to == "BRL" {
+        convertedAmount = decimal.NewFromFloat(amount).Mul(rate)
+    } else {
+        http.Error(w, "Invalid conversion", http.StatusBadRequest)
         return
     }
 
-    response := Response{
-        ValorConvertido: result,
-        SimboloMoeda:    getSymbol(to),
-    }
-
-    saveConversion(amount, from, to, rate, result)
-
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(response)
-}
+    // Criar uma nova conversão
+    conv := Conversion{
+        FromCurrency:    from,
+        ToCurrency:      to,
+        Rate:
